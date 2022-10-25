b65c28cb57acce8b88463c6afb8acff7e90430d2
Jianwen Pi <jianwen.pi@lookout.com>
2022-10-24 21:34:18 -0700

    remove polarssl

--- Makefile
+++ Makefile
@@ -1,11 +1,8 @@
 libssl:
-	gcc -o server ssl_server_libssl.c -lssl
-	gcc -o client ssl_client_libssl.c -lssl
-polarssl:
-	gcc -o server ssl_server_polarssl.c -lpolarssl -L../lib -I../include
-	gcc -o client ssl_client_polarssl.c -lpolarssl -L../lib -I../include
+	gcc -g -o server ssl_server_libssl.c -lssl -lcrypto
+	gcc -g -o client ssl_client_libssl.c -lssl -lcrypto
 
 clean:
 	rm -rf server client
-	
+
 
--- ssl_client_libssl.c
+++ ssl_client_libssl.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <sys/un.h>
+#include <netinet/in.h>
 #include <unistd.h>
 #include <string.h>
 
 #include <openssl/ssl.h>
 #include <openssl/err.h>
 
-#define SSL_CLIENT_RSA_CERT	"/home/nmathew/cacert/ssl_client.crt"
-#define SSL_CLIENT_RSA_KEY	"/home/nmathew/cacert/ssl_client.key"
-#define SSL_CLIENT_RSA_CA_CERT	"/home/nmathew/cacert/ca.crt"
-#define SSL_CLIENT_RSA_CA_PATH	"/home/nmathew/cacert/"
+//#define SSL_CLIENT_RSA_CERT	"/home/ubuntu/Projects/SSL-TLS-clientserver/certs/ssl_client.crt"
+//#define SSL_CLIENT_RSA_KEY	"/home/ubuntu/Projects/SSL-TLS-clientserver/certs/ssl_client.key"
+//#define SSL_CLIENT_RSA_CA_CERT	"/home/ubuntu/Projects/SSL-TLS-clientserver/certs/ca.crt"
 
-#define SSL_SERVER_ADDR		"/home/nmathew/ssl_server"
+#define SSL_CLIENT_RSA_CERT "/tmp/certs/client-cert.pem"
+#define SSL_CLIENT_RSA_KEY  "/tmp/certs/client-key.pem"
+#define SSL_CLIENT_RSA_CA_CERT "/tmp/certs/ca-cert.pem"
+
+#define PORT 5000
 
 #define OFF	0
 #define ON	1
@@ -24,7 +27,7 @@ int main(void)
 	SSL_METHOD *client_meth;
 	SSL_CTX *ssl_client_ctx;
 	int clientsocketfd;
-	struct sockaddr_un serveraddr;
+	struct sockaddr_in serveraddr;
 	int handshakestatus;
 	SSL *clientssl;
 	char buffer[1024] = "Client Hello World";
@@ -32,9 +35,9 @@ int main(void)
 
 	SSL_library_init();
 	SSL_load_error_strings();
-	client_meth = SSLv3_client_method();
+	client_meth = SSLv23_client_method();
 	ssl_client_ctx = SSL_CTX_new(client_meth);
-	
+
 	if(!ssl_client_ctx)
 	{
 		ERR_print_errors_fp(stderr);
@@ -42,49 +45,49 @@ int main(void)
 	}
 
 	if(verify_peer)
-	{	
-	
-		if(SSL_CTX_use_certificate_file(ssl_client_ctx, SSL_CLIENT_RSA_CERT, SSL_FILETYPE_PEM) <= 0)	
+	{
+
+		if(SSL_CTX_use_certificate_file(ssl_client_ctx, SSL_CLIENT_RSA_CERT, SSL_FILETYPE_PEM) <= 0)
 		{
 			ERR_print_errors_fp(stderr);
-			return -1;		
+			return -1;
 		}
 
-	
-		if(SSL_CTX_use_PrivateKey_file(ssl_client_ctx, SSL_CLIENT_RSA_KEY, SSL_FILETYPE_PEM) <= 0)	
+
+		if(SSL_CTX_use_PrivateKey_file(ssl_client_ctx, SSL_CLIENT_RSA_KEY, SSL_FILETYPE_PEM) <= 0)
 		{
 			ERR_print_errors_fp(stderr);
-			return -1;		
+			return -1;
 		}
-	
+
 		if(SSL_CTX_check_private_key(ssl_client_ctx) != 1)
 		{
 			printf("Private and certificate is not matching\n");
 			return -1;
-		}	
+		}
 
 		//See function man pages for instructions on generating CERT files
 		if(!SSL_CTX_load_verify_locations(ssl_client_ctx, SSL_CLIENT_RSA_CA_CERT, NULL))
 		{
 			ERR_print_errors_fp(stderr);
-			return -1;		
+			return -1;
 		}
 		SSL_CTX_set_verify(ssl_client_ctx, SSL_VERIFY_PEER, NULL);
 		SSL_CTX_set_verify_depth(ssl_client_ctx, 1);
 	}
 
-	if((clientsocketfd = socket(AF_UNIX, SOCK_STREAM, 0)) < 0)
+	if((clientsocketfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 	{
 		printf("Error on socket creation\n");
 		return -1;
 	}
-	memset(&serveraddr, 0, sizeof(struct sockaddr_un));
-	serveraddr.sun_family = AF_UNIX;
-	serveraddr.sun_path[0] = 0;
-	strncpy(&(serveraddr.sun_path[1]), SSL_SERVER_ADDR, strlen(SSL_SERVER_ADDR) + 1);
-	
-	connect(clientsocketfd, (struct sockaddr *)&serveraddr, sizeof(struct sockaddr_un));
-		
+	memset(&serveraddr, 0, sizeof(struct sockaddr_in));
+	serveraddr.sin_family = AF_INET;
+    serveraddr.sin_addr.s_addr = inet_addr("172.17.0.36");
+    serveraddr.sin_port = htons(PORT);
+
+	connect(clientsocketfd, (struct sockaddr *)&serveraddr, sizeof(struct sockaddr_in));
+
 
 	clientssl = SSL_new(ssl_client_ctx);
 	if(!clientssl)
@@ -93,29 +96,29 @@ int main(void)
 		return -1;
 	}
 	SSL_set_fd(clientssl, clientsocketfd);
-		
+
 	if((ret = SSL_connect(clientssl)) != 1)
 	{
 		printf("Handshake Error %d\n", SSL_get_error(clientssl, ret));
 		return -1;
 	}
-		
+
 	if(verify_peer)
 	{
 		X509 *ssl_client_cert = NULL;
 
 		ssl_client_cert = SSL_get_peer_certificate(clientssl);
-			
+
 		if(ssl_client_cert)
 		{
 			long verifyresult;
 
 			verifyresult = SSL_get_verify_result(clientssl);
 			if(verifyresult == X509_V_OK)
-				printf("Certificate Verify Success\n"); 
+				printf("Certificate Verify Success\n");
 			else
-				printf("Certificate Verify Failed\n"); 
-			X509_free(ssl_client_cert);				
+				printf("Certificate Verify Failed\n");
+			X509_free(ssl_client_cert);
 		}
 		else
 			printf("There is no client certificate\n");
@@ -127,5 +130,5 @@ int main(void)
 	close(clientsocketfd);
 	SSL_free(clientssl);
 	SSL_CTX_free(ssl_client_ctx);
-	return 0;	
+	return 0;
 }
